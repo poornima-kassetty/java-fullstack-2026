c3ed329b1b189e1a313b8d6ff0aa2353aa01afaa
poornima-kassetty <mails4poorni@gmail.com>
2026-05-21 22:41:46 +0530

    Day 2 : calculator with scanner

--- src/main/java/Day02_Calculator.java
+++ src/main/java/Day02_Calculator.java
@@ -1,19 +1,27 @@
+import java.util.Scanner;
+
 public class Day02_Calculator {
     public static void main(String args[])
     {
         int x, y,p,q;
-        x=110;
-        y=12;
-        p=++x;
+        //x=110;
+        //y=12;
+        System.out.println("====Scanner calculator=====");
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter value of x :");
+        x=sc.nextInt();
+        System.out.print("Enter value of y :");
+        y=sc.nextInt();
+       // p=++x;
         //q=++y;
-        System.out.println("====Hard coded calculator=====");
+       // System.out.println("====Hard coded calculator=====");
         System.out.println("value of x: "+x);
         System.out.println("value of y: "+y);
         System.out.println("sum of x and y: " + (x+y));
         System.out.println("Product of x and y : "+(x*y));
         System.out.println("value of (x/y) :"+(x/y));
         System.out.println("value of x%y :" +(x%y));
-       System.out.println("increment of x by 1: "+p);
+       System.out.println("increment of x by 1: "+ ++x);
         System.out.println("increment of y by 1: "+ ++y);
         System.out.println("decrement of x by 1: "+ --x);
         System.out.println("Decrement of y by 1: "+ --y);
