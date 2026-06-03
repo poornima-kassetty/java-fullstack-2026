7fa28f06fc9f4ea1adde977793fde0904fa7af51
poornima-kassetty <mails4poorni@gmail.com>
2026-06-03 10:39:00 +0530

    Practiced If... else statement

--- src/main/java/Day03_Ifcond.java
+++ src/main/java/Day03_Ifcond.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 
 public class Day03_Ifcond {
     public static void main(String args[]) {
-        System.out.print("Is weather Raining : if yes press 1 if not press 2  :  2");
+        System.out.print("Is weather Raining : if yes press 1 if not press 2  :  ");
         Scanner sc = new Scanner(System.in);
         int i = sc.nextInt();
 
         if (i == 1)
             System.out.println("Take umbrella");
+        else
+            System.out.println("No need to take umbrella");
 
         System.out.println("HAVE A NICE DAY");
 
